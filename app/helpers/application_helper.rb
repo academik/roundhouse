554f98c9c6aa226d353b4941f519a6e2361e17dd
Andrew <andrewdkippen@gmail.com>
2013-12-06 15:48:49 -0500

    Update application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
-end
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Roundhouse Poetry"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end # --- def full_title --- 
+end # --- Module --- 
