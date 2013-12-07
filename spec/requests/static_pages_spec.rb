ae7210d06e0680cbb40810a7f1fb632d3221d5b5
Andrew <andrewdkippen@gmail.com>
2013-12-06 22:59:40 -0500

    Update links & add titles to poems /poets pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -24,5 +24,24 @@ describe "StaticPages" do
     it { should have_title(full_title('Contact')) }
   end # --- describe "Contact Page" do --- 
 
+  it "should have the right links on the layout" do
+    visit root_path
+    
+    click_link "Poets"
+    expect(page).to have_title(full_title('Poets'))
+
+    click_link "Poems"
+    expect(page).to have_title(full_title('Poems'))
+
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+
+  end
+
+
+
 
 end # --- describe "StaticPages" do --- 
