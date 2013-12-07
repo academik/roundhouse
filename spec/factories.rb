f6e0a4d9bd74188f6a8ac8720cfadb17e99b6c07
Andrew <andrewdkippen@gmail.com>
2013-12-07 01:12:44 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Michael Hartl"
+    username "mhartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
