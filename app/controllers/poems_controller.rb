aa6623f395b90fedcae636b1f2e6795203f64119
Andrew <andrewdkippen@gmail.com>
2013-12-16 10:47:19 -0500

    update Poem Index page

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -4,7 +4,7 @@ class PoemsController < ApplicationController
   # GET /poems
   # GET /poems.json
   def index
-    @poems = Poem.all
+    @poems = Poem.reorder("title ASC").paginate(page: params[:page], :per_page => 25)
   end
 
   # GET /poems/1
